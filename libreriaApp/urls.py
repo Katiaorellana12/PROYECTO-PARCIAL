fbcee604b136e91019f1d402f1b674bf4a3c96b6
Katiaorellana12 <katia2orellana@gmail.com>
2022-11-20 16:31:21 -0600

    add: Styles

--- libreriaApp/urls.py
+++ libreriaApp/urls.py
@@ -1,11 +1,16 @@
 
 from django.urls import path
 from .views import *
+from .views import Home
 
 urlpatterns = [
     
-    path('index/', index), 
+    path('', Home.as_view(), name="home"), 
+    path('login/', auth_views.LoginView.as_view(template_name='libreriaorellana/login.html'), name='login'), 
     path('about/' , about), 
     path('about2/' , about2), 
-    path('lte/',  lte), 
+    path('lte/',  lte),
+    path('index/',  index), 
+    path('footer/',  footer), 
+
 ]
--- libreriaApp/views.py
+++ libreriaApp/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
 
 # Create your views here.
-def index (request):
-    return render(request, "index.html")
+class Home(LoginRequiredMixin , generic.TemplateView):
+    template_name = 'libreriaorellana/index.html'
+    login_url = '/admin'
+
+class Home(LoginRequiredMixin , generic.TemplateView):
+    template_name = 'libreriaorellana/index.html'
+    login_url = 'lib:login'
 
 def about(request):
     return render(request, "about.html")
@@ -13,3 +21,10 @@ def about2(request):
 
 def lte(request):
     return render(request, "lte.html")  
+
+
+def index(request):
+    return render(request, "index.html")
+
+def footer(request):
+    return render(request, "footer.html")
